d83b9a1d75063d9d8de531dd8c0a1237c27d2a4b
Anish <mittal.software@gmail.com>
2021-05-20 19:11:40 +0530

    Code changes for HandlerInterceptor

--- api-authenticator/src/main/java/com/upwork/apiauthenticator/controller/PropertyController.java
+++ api-authenticator/src/main/java/com/upwork/apiauthenticator/controller/PropertyController.java
@@ -3,6 +3,7 @@ package com.upwork.apiauthenticator.controller;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,11 +11,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.upwork.apiauthenticator.entities.Property;
 import com.upwork.apiauthenticator.service.PropertyService;
+import com.upwork.apiauthenticator.util.Utility;
 
 @RestController
 @RequestMapping("/api/property")
